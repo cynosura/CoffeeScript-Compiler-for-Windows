858329ff26a8977f0a80f809b986193830e9ed97
Ray Glover <Ray@NAUSICA>
2011-04-22 18:44:23 +0100

    fix coffeescript 'bare' compiler option

--- coffee/coffee-stdinout.js
+++ coffee/coffee-stdinout.js
@@ -8,13 +8,14 @@ Exec(thisDir + 'coffee-script.js');
 var retries = 3;
 var script;
 
-do {	
+do {
+	
 	var script = File.stdin.Read(undefined)
 } while (script != undefined && 
          script.length <= 0 && 
 		 retries >= 0)
 
+
 File.stdout.Write(
 	  CoffeeScript.compile(
-		script,
-		{noWrap: false}));
+		script, {bare: true}));
