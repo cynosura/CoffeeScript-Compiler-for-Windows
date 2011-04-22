65bbfe11fa169e700a6b6adc696628d9674b2ef1
Ray Glover <Ray@NAUSICA>
2011-04-22 16:00:27 +0100

    stdinout script allows applications to compile coffeescript as a child process

---
+++ coffee/coffee-stdinout.js
@@ -0,0 +1,20 @@
+LoadModule('jsstd');
+LoadModule('jsio');
+
+var thisDir = arguments[0].replace(/[^\\/]+$/, '');
+
+Exec(thisDir + 'coffee-script.js');
+
+var retries = 3;
+var script;
+
+do {	
+	var script = File.stdin.Read(undefined)
+} while (script != undefined && 
+         script.length <= 0 && 
+		 retries >= 0)
+
+File.stdout.Write(
+	  CoffeeScript.compile(
+		script,
+		{noWrap: false}));
